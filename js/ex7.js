bb9db427e63d51822ed2b1b5374c8e0ec8bb01b5
Rana-rene <rene.trujillo01@student.csulb.edu>
2022-10-13 18:19:25 -0700

    Final

--- js/ex7.js
+++ js/ex7.js
@@ -4,13 +4,30 @@ let num2 = prompt(`Please enter another number you would like to add:`)
 const n1Int = Number(num1);
 const n2Int = Number(num2);
 
+const paraElement = document.createElement("p");
 const spanElement = document.createElement("span");
+paraElement.id = "para";
 
 if (Number.isInteger(n1Int) && Number.isInteger(n2Int)) {
-    let numSum = n1Int + n2Int; 
+    let numSum = n1Int + n2Int;
     let texts = (`${num1} + ${num2} = `);
-    spanElement.textContent = (`${num1} + ${num2} = ${numSum}`);
-    spanElement.style.fontWeight= 'bold';
+    paraElement.textContent = (`${num1} + ${num2} = `);
+    paraElement.style.fontWeight = 'bold';
+    spanElement.textContent = (` ${numSum}`);
+    spanElement.style.fontWeight = 'bold';
+    spanElement.style.color = 'red';
+    document.getElementById("content").appendChild(paraElement);
+    document.getElementById("para").appendChild(spanElement);
     console.log(texts);
 }
-document.getElementById("content").appendChild(spanElement);
+else {
+    paraElement.textContent = "Error!";
+    spanElement.textContent = (` You must enter integers. You entered "${num1}" and "${num2}". Try again.`);
+    paraElement.style.fontWeight = 'bold';
+    paraElement.style.color = 'red';
+    spanElement.style.color = 'black';
+    document.getElementById("content").appendChild(paraElement);
+    document.getElementById("para").appendChild(spanElement);
+}
+
+
--- js/test.js
+++
@@ -1,3 +0,0 @@
-for (var i = 0, row; row = table.rows[i]; i++) {
-    console.log(row.innertext)
-}
